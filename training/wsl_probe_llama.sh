e5dfb6da9680791c9c39bf58f727d6174245c781
Sanjtih-KM-Cyber <sanjith.k.m12@gmail.com>
2026-09-24 08:03:38 +0530

    Remove one-off probe script

--- training/wsl_probe_llama.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for name in llama-v0.4.1-bin-ubuntu-x64.tar.gz llama-b6485-bin-ubuntu-x64.tar.gz; do
-  code=$(curl -sSL -o /tmp/probe.bin -w "%{http_code}:%{size_download}" "https://github.com/ggml-org/llama.cpp/releases/download/v0.4.1/$name")
-  echo "$name -> $code"
-  head -c 60 /tmp/probe.bin | head -1
-done
-echo "---DONE---"
